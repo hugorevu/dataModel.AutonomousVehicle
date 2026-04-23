b9fe65a893413b17c07f3492d1e4cda396a3e332
hugorevu <hra965@alumnos.unican.es>
2026-04-23 12:24:59 +0200

    add sensor gnss

--- FIWARE_DEVICE_MODEL/examples/init.bat
+++ FIWARE_DEVICE_MODEL/examples/init.bat
@@ -5,20 +5,35 @@ curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
 -H "Content-Type: application/ld+json" ^
 -d @autonomousVehicle.json
 
-echo Creating DeviceModel...
-curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
--H "Content-Type: application/ld+json" ^
--d @deviceModelCamera.json
-
 echo Creating Device...
 curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
 -H "Content-Type: application/ld+json" ^
 -d @deviceCamera.json
 
+echo Creating DeviceModel...
+curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
+-H "Content-Type: application/ld+json" ^
+-d @deviceModelCamera.json
+
 echo Creating Measurement...
 curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
 -H "Content-Type: application/ld+json" ^
 -d @deviceMeasurementCamera.json
+
+echo Creating Device for GNSS...
+curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
+-H "Content-Type: application/ld+json" ^
+-d @deviceGNSS.json
+
+echo Creating DeviceModel for GNSS...
+curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
+-H "Content-Type: application/ld+json" ^
+-d @deviceModelGNSS.json
+
+echo Creating Measurement for GNSS...
+curl -X POST http://localhost:8080/ngsi-ld/v1/entities ^
+-H "Content-Type: application/ld+json" ^
+-d @deviceMeasurementGNSS.json
 
 echo Done.
 pause
